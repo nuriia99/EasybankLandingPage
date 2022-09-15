e22241ca56a0c5801e42e53be0aaba916ec09978
nuria <nurianton1999@gmail.com>
2022-09-15 13:00:04 +0200

    header finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log("a")
+// const btnHam = document.querySelector("#btnHam");
+const btnHam = document.querySelector("#btn-ham");
+btnHam.addEventListener('click', ()=>{
+    if(btnHam.classList.contains("header-menu-open")){
+        btnHam.classList.remove("header-menu-open");
+    }
+    else btnHam.classList.add("header-menu-open");
+});
+
