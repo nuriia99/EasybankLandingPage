85e39822579d06d805e8e636859ee1632d47580c
nuria <nurianton1999@gmail.com>
2022-09-16 21:21:53 +0200

    menu finished

--- app/js/script.js
+++ app/js/script.js
@@ -3,11 +3,12 @@ const btnHam = document.querySelector("#btn-ham");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade");
-
+const body = document.querySelector("body");
 
 
 btnHam.addEventListener('click', ()=>{
     if(header.classList.contains("open")){
+        body.classList.remove("noscroll");
         header.classList.remove("open");
         fadeElements.forEach((element)=>{
             element.classList.add("fade-out");
@@ -16,6 +17,7 @@ btnHam.addEventListener('click', ()=>{
         
     }
     else{
+        body.classList.add("noscroll");
          header.classList.add("open");
          fadeElements.forEach((element)=>{
             element.classList.remove("fade-out");
