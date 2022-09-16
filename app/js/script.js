d58e56e6d2a868116d83e1c29af970fcc771dac5
nuria <nurianton1999@gmail.com>
2022-09-16 11:32:57 +0200

    animations done

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,16 @@
 // const btnHam = document.querySelector("#btnHam");
 const btnHam = document.querySelector("#btn-ham");
+const overlay = document.querySelector(".overlay");
 btnHam.addEventListener('click', ()=>{
-    if(btnHam.classList.contains("header-menu-open")){
-        btnHam.classList.remove("header-menu-open");
+    if(btnHam.classList.contains("open")){
+        btnHam.classList.remove("open");
+        overlay.classList.add("fade-out");
+        overlay.classList.remove("fade-in");
     }
-    else btnHam.classList.add("header-menu-open");
+    else{
+         btnHam.classList.add("open");
+         overlay.classList.remove("fade-out");
+         overlay.classList.add("fade-in");
+    }
 });
 
