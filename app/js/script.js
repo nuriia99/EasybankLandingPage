4df5502567b85be747c1a573157199101727e687
nuria <nurianton1999@gmail.com>
2022-09-30 12:50:34 +0200

    animation hero almost finished

--- app/js/script.js
+++ app/js/script.js
@@ -4,8 +4,21 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade");
 const body = document.querySelector("body");
+const toLoadElements = document.querySelectorAll(".to-load");
+
+//on-load
+// window.onload = function() {
+//     console.log(toLoadElements);
+//     setTimeout(function(){
+//         toLoadElements.forEach((element)=>{
+//             element.classList.remove("to-load");
+//             element.classList.add("load");
+//          });
+//     }, 0);
+//   };
 
 
+// hamburguer menu
 btnHam.addEventListener('click', ()=>{
     if(header.classList.contains("open")){
         body.classList.remove("noscroll");
