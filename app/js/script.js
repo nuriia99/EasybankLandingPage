a2bf78f5f6b6ba5e9461d3bdebfad42d0fd3cc10
nuria <nurianton1999@gmail.com>
2022-10-04 18:46:14 +0200

    elint and error lens

--- app/js/script.js
+++ app/js/script.js
@@ -1,58 +1,37 @@
-// const btnHam = document.querySelector("#btnHam");
-const btnHam = document.querySelector("#btn-ham");
-const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
-const fadeElements = document.querySelectorAll(".has-fade");
-const body = document.querySelector("body");
-const toLoadElements = document.querySelectorAll(".to-load");
-
-//on-load
-// window.onload = function() {
-//     console.log(toLoadElements);
-//     setTimeout(function(){
-//         toLoadElements.forEach((element)=>{
-//             element.classList.remove("to-load");
-//             element.classList.add("load");
-//          });
-//     }, 0);
-//   };
-
+// const btnHam = document.querySelector('#btnHam')
+const btnHam = document.querySelector('#btn-ham')
+const header = document.querySelector('.header')
+const fadeElements = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
 
 // hamburguer menu
-btnHam.addEventListener('click', ()=>{
-    if(header.classList.contains("open")){
-        console.log("llego")
-        body.classList.remove("noscroll");
-        header.classList.remove("open");
-        fadeElements.forEach((element)=>{
-            element.classList.add("fade-out");
-            element.classList.remove("fade-in");
-         });
-        
-    }
-    else{
-        body.classList.add("noscroll");
-         header.classList.add("open");
-         fadeElements.forEach((element)=>{
-            element.classList.remove("fade-out");
-            element.classList.add("fade-in");
-         });
-         
-    }
-});
-
+btnHam.addEventListener('click', () => {
+  if (header.classList.contains('open')) {
+    body.classList.remove('noscroll')
+    header.classList.remove('open')
+    fadeElements.forEach((element) => {
+      element.classList.add('fade-out')
+      element.classList.remove('fade-in')
+    })
+  } else {
+    body.classList.add('noscroll')
+    header.classList.add('open')
+    fadeElements.forEach((element) => {
+      element.classList.remove('fade-out')
+      element.classList.add('fade-in')
+    })
+  }
+})
+// close the hamburger menu when the screen is bigger
 visualViewport.onresize = () => {
-    if(visualViewport.width > 1008){
-        if(header.classList.contains("open")){
-            body.classList.remove("noscroll");
-            header.classList.remove("open");
-            fadeElements.forEach((element)=>{
-                element.classList.add("fade-out");
-                element.classList.remove("fade-in");
-             });
-            
-        }
+  if (visualViewport.width > 1008) {
+    if (header.classList.contains('open')) {
+      body.classList.remove('noscroll')
+      header.classList.remove('open')
+      fadeElements.forEach((element) => {
+        element.classList.add('fade-out')
+        element.classList.remove('fade-in')
+      })
     }
-    
-  };
-
+  }
+}
